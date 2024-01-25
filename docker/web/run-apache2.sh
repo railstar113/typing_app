53afe5eb0571b10bb6912287624fdc6a14e0c83f
railstar113 <kodama113@gmail.com>
2024-01-25 17:36:24 +0900

    run-apache2.shの修正

--- docker/web/run-apache2.sh
+++ docker/web/run-apache2.sh
@@ -5,5 +5,6 @@ sed -ri -e "182a\DocumentRoot ${APACHE_DOCUMENT_ROOT}" /etc/apache2/apache2.conf
 sed -ri -e "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/ports.conf
 rm /etc/apache2/mods-enabled/mpm_event.conf
 rm /etc/apache2/mods-enabled/mpm_event.load
+cd /opt/typing_app/src
 composer install
 apache2-foreground "$@"
