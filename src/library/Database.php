55264f323860ac94d1e6a07452baab1cfe03776d
railstar113 <kodama113@gmail.com>
2024-01-26 19:56:05 +0900

    Database.phpのタイムゾーン修正

--- src/library/Database.php
+++ src/library/Database.php
@@ -21,7 +21,7 @@ class Database
             \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
             \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ,
             \PDO::ATTR_EMULATE_PREPARES => false,
-            \PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone='+09:00'",
+            \PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone = '+09:00'",
           ]
         );
       }
